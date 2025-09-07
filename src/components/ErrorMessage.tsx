e3661e3a023020a6dd41ecc83809a2321cba508c
Tanjim605 <tanjimulislam2001@gmail.com>
2025-09-07 10:22:09 +0600

    Error message children prop done.

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,7 @@
-export default function ErrorMessage({ children }) {
+type ErrorMessageProps = {
+  children: React.ReactNode;
+};
+
+export default function ErrorMessage({ children }: ErrorMessageProps) {
   return <div className="text-center text-red-500">{children}</div>;
 }
